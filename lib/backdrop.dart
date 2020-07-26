0df0c6ec1422e21e3b529e94707629b7a0564eb4
Aykut Önen <aykutonen@gmail.com>
2020-07-27 01:27:34 +0300

    .

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -3,7 +3,8 @@ import 'package:meta/meta.dart';
 
 import 'model/product.dart';
 
-// TODO: Add velocity constant (104)
+// Add velocity constant (104)
+const double _kFlingVelocity = 2.0;
 
 class BackDrop extends StatefulWidget {
   final Category currentCategory;
@@ -28,24 +29,104 @@ class BackDrop extends StatefulWidget {
   _BackDropState createState() => _BackDropState();
 }
 
-// TODO: Add _FrontLayer class (104)
+// Add _FrontLayer class (104)
+class _FrontLayer extends StatelessWidget {
+  // TODO: Add on-tap callback (104)
+
+  final Widget child;
+
+  const _FrontLayer({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 16.0,
+      shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(46.0)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // TODO: Add a GestureDetector (104)
+          Expanded(
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 // TODO: Add _BackdropTitle class (104)
 
-// TODO: Add _BackdropState class (104)
+// Add _BackdropState class (104)
 class _BackDropState extends State<BackDrop>
     with SingleTickerProviderStateMixin {
   final GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
 
-  // TODO: Add AnimationController widget (104)
+  // Add AnimationController widget (104)
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 300),
+      value: 1.0,
+      vsync: this,
+    );
+  }
+
+  // TODO: Add override for didUpdateWidget (104)
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // Add functions to get and change front layer visibility (104)
+  bool get _FrontLayerVisible {
+    final AnimationStatus status = _controller.status;
+    return status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+  }
+
+  void _toggleBackdropLayerVisibility() {
+    _controller.fling(
+        velocity: _FrontLayerVisible ? -_kFlingVelocity : _kFlingVelocity);
+  }
 
   // TODO: Add BuildContext and BoxConstraints parameters to _buildStack (104)
-  Widget _buildStack() {
+  Widget _buildStack(BuildContext context, BoxConstraints constraints) {
+    const double layerTitleHeight = 48.0;
+    final Size layerSize = constraints.biggest;
+    final double layerTop = layerSize.height - layerTitleHeight;
+
+    // TODO: Create a RelativeRectTween Animation (104)
+    Animation<RelativeRect> layerAnimation = RelativeRectTween(
+      begin: RelativeRect.fromLTRB(
+          0.0, layerTop, 0.0, layerTop - layerSize.height),
+      end: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
+    ).animate(_controller.view);
+
     return Stack(
       key: _backdropKey,
       children: <Widget>[
-        // TODO: Wrap backLayer in an ExcludeSemantics widget (104)
-        widget.backLayer,
-        widget.frontLayer
+        // Wrap backLayer in an ExcludeSemantics widget (104)
+        ExcludeSemantics(
+          child: widget.backLayer,
+          excluding: _FrontLayerVisible,
+        ),
+        // Add a PositionedTransition (104)
+        PositionedTransition(
+          rect: layerAnimation,
+          child: _FrontLayer(child: widget.frontLayer),
+        ),
+        // Wrap front layer in _FrontLayer (104)
       ],
     );
   }
@@ -56,10 +137,13 @@ class _BackDropState extends State<BackDrop>
       brightness: Brightness.light,
       elevation: 0.0,
       titleSpacing: 0.0,
-      // TODO: Replace leading menu icon with IconButton (104)
+      // Replace leading menu icon with IconButton (104)
       // TODO: Remove leading property (104)
       // TODO: Create title with _BackdropTitle parameter (104)
-      leading: Icon(Icons.menu),
+      leading: IconButton(
+        icon: Icon(Icons.menu),
+        onPressed: _toggleBackdropLayerVisibility,
+      ),
       title: Text('SHRINE'),
       actions: <Widget>[
         // TODO: Add shortcut to login screen from trailing icons (104)
@@ -86,7 +170,7 @@ class _BackDropState extends State<BackDrop>
 
     return Scaffold(
       appBar: appBar,
-      body: _buildStack(),
+      body: LayoutBuilder(builder: _buildStack),
     );
   }
 }
--- lib/supplemental/product_columns.dart
+++ lib/supplemental/product_columns.dart
@@ -33,13 +33,14 @@ class TwoProductCardColumn extends StatelessWidget {
 
       double heightOfCards = (constraints.biggest.height - spacerHeight) / 2.0;
       double heightOfImages = heightOfCards - ProductCard.kTextBoxHeight;
-      // TODO: Change imageAspectRatio calculation (104)
-      double imageAspectRatio = constraints.biggest.width / heightOfImages;
+      // Change imageAspectRatio calculation (104)
+      double imageAspectRatio = heightOfImages >= 0.0
+          ? constraints.biggest.width / heightOfImages
+          : 49.0 / 33.0;
 
-      // TODO: Replace Column with a ListView (104)
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      // Replace Column with a ListView (104)
+      return ListView(
+        physics: const ClampingScrollPhysics(),
         children: <Widget>[
           Padding(
             padding: EdgeInsetsDirectional.only(start: 28.0),
@@ -62,6 +63,32 @@ class TwoProductCardColumn extends StatelessWidget {
           ),
         ],
       );
+
+      // return Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   crossAxisAlignment: CrossAxisAlignment.center,
+      //   children: <Widget>[
+      //     Padding(
+      //       padding: EdgeInsetsDirectional.only(start: 28.0),
+      //       child: top != null
+      //           ? ProductCard(
+      //               imageAspectRatio: imageAspectRatio,
+      //               product: top,
+      //             )
+      //           : SizedBox(
+      //               height: heightOfCards,
+      //             ),
+      //     ),
+      //     SizedBox(height: spacerHeight),
+      //     Padding(
+      //       padding: EdgeInsetsDirectional.only(end: 28.0),
+      //       child: ProductCard(
+      //         imageAspectRatio: imageAspectRatio,
+      //         product: bottom,
+      //       ),
+      //     ),
+      //   ],
+      // );
     });
   }
 }
@@ -73,15 +100,16 @@ class OneProductCardColumn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Replace Column with a ListView (104)
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
+    // Replace Column with a ListView (104)
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      reverse: true,
       children: <Widget>[
-        ProductCard(
-          product: product,
-        ),
         SizedBox(
           height: 40.0,
+        ),
+        ProductCard(
+          product: product,
         ),
       ],
     );
